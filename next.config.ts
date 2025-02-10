25bb79a981fce6695fd6c7cf50ec10765a77387d
LateNever <kirsharik@gmail.com>
2025-02-11 02:33:06 +0500

    пробуем загрузить на gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Jan2025TaskManager',
+  assetPrefix: '/Jan2025TaskManager',
 };
 
 export default nextConfig;
